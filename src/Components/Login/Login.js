eeee5998d61904fdc6cbb423006447385028249b
Cuthbert20 <knowltonspencer@gmail.com>
2019-11-19 18:06:33 -0700

    add useEffect hook

--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -2,18 +2,18 @@ import React, { useState, useEffect } from "react";
 import axios from "axios";
 
 function Login() {
-  const [username, setUsername] = useState("Spencer");
+  const [username, setUsername] = useState("");
+  useEffect(() => {
+    document.title = `${username}'s Study Card App`;
+  });
   const handleChange = (e, key) => {
     setUsername(e.target.value);
   };
+  console.log(username);
   return (
     <div>
       <h1>Login Component</h1>
-      <input
-        onChange={e => handleChange(e, "username")}
-        type="text"
-        value={username}
-      />
+      <input onChange={e => handleChange(e)} type="text" value={username} />
     </div>
   );
 }
