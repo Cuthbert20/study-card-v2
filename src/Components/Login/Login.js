04aedc0d56285b882363ad954707f265b3599922
Cuthbert20 <knowltonspencer@gmail.com>
2019-11-20 20:26:32 -0700

    adding register function

--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -1,19 +1,32 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
+import Register from "../Register/Register";
 
 function Login() {
   const [username, setUsername] = useState("");
   useEffect(() => {
-    document.title = `${username}'s Study Card App`;
+    document.title = `${username}'s Study Cards`;
   });
   const handleChange = (e, key) => {
     setUsername(e.target.value);
   };
   console.log(username);
+  function handleRegister() {
+    console.log("hit");
+  }
   return (
     <div>
       <h1>Login Component</h1>
-      <input onChange={e => handleChange(e)} type="text" value={username} />
+      <input
+        placeholder="username"
+        onChange={e => handleChange(e)}
+        type="text"
+        value={username}
+      />
+      <input type="text" placeholder="password" />
+      <button>Submit</button>
+      <button onClick={() => handleRegister()}>Register</button>
+      {/* conditionally rendering the Register Component */}
     </div>
   );
 }
